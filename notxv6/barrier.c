85de11224d6d1312422101a1cc5c72a3f71085ef
yeelight <91862060+FeiNiaoBF@users.noreply.github.com>
2024-01-09 20:26:22 +0800

    Done lab thread---Barrier, time spent: ~5h

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -22,15 +22,32 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
-static void 
+static void
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+    // YOUR CODE HERE
+    //
+    // Block until all threads have called barrier() and
+    // then increment bstate.round.
+    //
+
+    pthread_mutex_lock(&bstate.barrier_mutex);
+
+    bstate.nthread++; // Increment the number of threads that have reached this round of the barrier
+    if (bstate.nthread == nthread)
+    {
+        bstate.round++;
+        bstate.nthread = 0; // reset thread
+        // wakeup all threads
+        pthread_cond_broadcast(&bstate.barrier_cond);
+    }
+    else
+    {
+        // thread sleep
+        pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+
+    pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
